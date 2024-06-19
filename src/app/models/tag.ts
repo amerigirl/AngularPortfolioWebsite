292951fb25e8b65e1c9259e80f01d8e7b12d9ec4
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-06-18 23:13:05 -0700

    added images to portfolio cards!

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -1,21 +1,22 @@
 export class Tag {
   static readonly ANGULAR = new Tag('Angular', 'red');
   static readonly TYPESCRIPT = new Tag('Typescript', ' blue');
-  static readonly HTML = new Tag('HTML', 'orange');
-  static readonly JAVASCRIPT = new Tag('Javascript', 'light green');
-  static readonly CSHARP = new Tag('C#', 'green');
+  static readonly HTML = new Tag('HTML', 'brown');
+  static readonly JAVASCRIPT = new Tag('Javascript', 'green');
+  static readonly CSHARP = new Tag('C-Sharp', 'green');
   static readonly JAVA = new Tag('Java', 'violet');
   static readonly ASPNET = new Tag('ASP.Net', 'teal');
   static readonly PYTHON = new Tag('Python', 'brown');
-  static readonly NODEJS = new Tag('Node.JS', 'orange');
+  static readonly NODEJS = new Tag('Node', 'orange');
+  static readonly SQL = new Tag('SQL', 'orange');
 
   private constructor(
     private readonly key: string,
     public readonly color: string
   ) {}
 
-  toString() {  //this will help make the keys READABLE when we move it over to html
+  toString() {
+    //this will help make the keys READABLE when we move it over to html
     return this.key;
   }
-
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,12 +14,34 @@ export class PortfolioComponent {
     {
       id: 0,
       name: 'JavaScript Practice Exercises',
-      summary: 'A leetcode problem a day keeps the fear away!',
+      summary: 'Leetcode practice problems :) ',
       description:
         'This is a compilation of easy and medium JavaScript practice exercises for coding interviews.  Data structures and algorithms are also great for building logic and reasoning skills.',
       projectLink: 'https://github.com/amerigirl/JavaScript-Practice-Exercises',
-      pictures: ['src/assets/images/leetcode.png'],
+      pictures: ['/assets/images/studying!.jpg'],
       tags: [Tag.JAVASCRIPT],
+    },
+    {
+      id: 0,
+      name: 'Student Admin Portal (F.E.)',
+      summary: 'Because students need things too!',
+      description:
+        'This app will give you an opportunity to view, create, update and delete students in a mock database. It will also allow you, once created, to upload an image that shows your student, along with the ability to change your image and basic demographic data at any time. This app uses the Angular framework with Angular Materials for styling. This is a basic CRUD app designed to give you practice with the foundational elements of working with Angular and connecting to an API to provide access to a database for persistent storage.',
+      projectLink:
+        'https://github.com/amerigirl/Student-Admin-Portal-with-Persistent-Storage',
+      pictures: ['/assets/images/SAportal.png'],
+      tags: [Tag.TYPESCRIPT, Tag.ANGULAR, Tag.HTML],
+    },
+    {
+      id: 0,
+      name: 'Student Admin Portal (B.E.)',
+      summary: 'Creating persistent storage with ASP.NET!',
+      description:
+        'As a companion repo to the Lets-Make-Some-New-Students repo, this repository will offer the connectivity needed to store your student data via a REST API created using ASP .NET Core. It uses automapper to map infomration coming from the front end securely to a SQL database for secure storage and retrival in the frontend for edits, etc.',
+      projectLink:
+        'https://github.com/amerigirl/Student-Admin-Portal-API-CompanionRepo',
+      pictures: ['/assets/images/backendwork.png'],
+      tags: [Tag.ASPNET, Tag.CSHARP, Tag.SQL],
     },
   ];
 
