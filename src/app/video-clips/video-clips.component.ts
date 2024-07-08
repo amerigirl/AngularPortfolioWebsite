6fdf8733995ca8580a7319d9ded5026fa5068187
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-07-08 15:01:34 -0700

    updated titleservice for video clips page

--- src/app/video-clips/video-clips.component.ts
+++ src/app/video-clips/video-clips.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-clips',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class VideoClipsComponent {
 
+
+  constructor(private titleservice: Title) {
+    this.titleservice.setTitle('Be-Asia McKerracher - Videos');
+ }
 }
