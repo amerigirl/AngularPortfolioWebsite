d50e9de05792824e277c892e210ec482462a55f2
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-08-29 15:26:35 -0700

    beginning to work with the resume section

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -7,8 +7,18 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./resume.component.css'],
 })
 export class ResumeComponent {
-  constructor(private titleservice: Title) {
+  //you need to add a renderer to deal with viewing files or downloading them??
+  constructor(private renderer: Renderer2,private titleservice: Title) {
     //this will set the tab title
     this.titleservice.setTitle('Be-Asia McKerracher - Resume');
+  }
+
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '/assets/images/resumePDF2024.pdf');
+    link.setAttribute('download', 'resumePDF2024.pdf');
+    link.click();
+    link.remove();
   }
 }
